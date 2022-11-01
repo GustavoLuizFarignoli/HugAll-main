36521dcced42f908e9e28503d64c7fc0ef50c241
GustavoLuizFarignoli <gustavolf.educ@gmail.com>
2022-11-01 16:31:14 -0300

    Arrumei a gigantesca quantidade de texto que tinha na página de sobre nós

--- Index.php
+++ Index.php
@@ -21,37 +21,35 @@
             <a href="#"></a>
         </div>
         <div class="body">
-            <div class="texto">
-                <div class="topico"></div>
-                    <h1>Quem Somos Nós?</h1>
+                <details>
+                    <summary><h1>Quem Somos Nós?</h1></summary>
                     <p>O Hugall é um projeto desenvolvido por um grupo de estudantes da PUCPR e tem como objetivo conectar Voluntários e Doadores com as ONGs de Curitiba</p>
-                </div>
-                <hr>
-                <div class="topico">
-                    <h2>Mas como fazemos Isso ?</h2>
+                    <hr>
+                </details>
+                <details>
+                    <summary><h1>Como Obtemos as informações apresentadas ?</h1></summary>
                     <p>O HugAll possui um base de dados com um resumo das informações que são possiveis de se encontrar sobre cada ONG e com as informações que nossos usuários
                         podem nos dar sobre elas e apresentamos elas de maneira simplificada para aqueles que não conheçem possam saber um pouquinho sobre essas instituições que
                         fazem o bem para tantas pessoas, mas que não possuem os recursos para ajudar todas as aqueles que precisam
                     </p>
-                </div>
-                <hr>
-                <div class="topico">
-                    <h2>Como participar e ajudar as ONGs ?</h2>
+                    <hr>
+                </details>
+                <details>
+                    <summary><h1>Como participar e ajudar as ONGs ?</h1></summary>
                     <p>O HugAll Serve apenas como uma vitrine, ao se cadastrar você pode encontrar as informações necessárias para se entrar em contanto com as ONGs assim podendo se
                         informar como cada instituição deseja receber ajuda, além disso o site possibilita que você entre em contato com outras pessoas engajadas na causa para conversar
                         para conhecer outras maneiras de como ajudar mais pessoas
                     </p>
-                </div>
-                <hr>
-                <div class="topico">
-                    <h2>Qual a vantagem de utilizar o HugAll ?</h2>
+                    <hr>
+                </details>
+                <details>
+                    <summary><h1>Por que utilizar o HugAll ?</h1></summary>
                     <p>O Nosso site possui um área de perfil em que você como usuário pode registrar as ajudas que você prestou a todas as ONGs que descobriu graças ao nosso site,
                         dessa meneira você possui portifolio certificado de tudo que você já fez para ajudar a melhorar a cidade
                     </p>
-                </div>
-                <hr>
+                    <hr>
+                </details>
                 <img id="image" src="Imagens/hugALL-removebg-preview.png">  
-            </div> 
         </div>
     </div>
     <script lang="javascript">
